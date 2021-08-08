393c4ade0561661d2be6b0a4ca07da63036bc448
pinkipie11 <85274828+pinkipie11@users.noreply.github.com>
2021-08-08 11:10:31 +0200

    Delete db.php

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$db=mysqli_connect('localhost', 'root', '', 'spa');
-mysqli_set_charset($db, "utf8");
-if(mysqli_connect_errno()){
-  echo 'Ne možete se konektirati na bazu';
-  exit;
-}
-?>
